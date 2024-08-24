a0c4cc2cef6af0f315c0c8b8227461726f270e61
Victor Giuliano <vitor.giuliano@gmail.com>
2024-08-24 20:17:26 -0300

    sum of three numbers

---
+++ sum_three_numbers.py
@@ -0,0 +1,6 @@
+a = float(input("Type the first number"))
+b = float(input("now the second"))
+c = float(input("now the third"))
+res = a + b + c
+
+print(f"The sum of the numbers {a} {a} {c} is {res}")
