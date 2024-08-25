c41cf1aa32025590e069557fa8473146c7c746cf
Victor Giuliano <vitor.giuliano@gmail.com>
2024-08-25 12:49:56 -0300

    sum of square of three numbers

---
+++ 01/read_print_three_squared.py
@@ -0,0 +1,6 @@
+a = float(input("Type number one: "))
+b = float(input("Type number two: "))
+c = float(input("Type number three: "))
+res = (a*a) + (b*c) + (c*c)
+
+print(f"Yours numbers is {a:.0f}, {b}. {c}\nThe sum of square of 3 numbers is {res}")
