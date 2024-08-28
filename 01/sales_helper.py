60ddc8bf4ca7649d5c85174265d62f47fca0f84d
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-08-28 16:11:09 -0300

    sales helper

---
+++ 01/sales_helper.py
@@ -0,0 +1,3 @@
+item = float(input("Type item price "))
+print(f"Item with 10% of descont: {item * 0.9} \nsalesman comission: {(item * 0.9) * 0.05}")
+print(f"Item in 3x: {item/3:.2f} \n salesman comission: {item * 0.05}")
