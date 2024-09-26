2287cb4957519db78599c96491ba25b81322759b
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-25 23:05:36 -0300

    test for while loop

---
+++ prova1p/test.py
@@ -0,0 +1,19 @@
+
+
+controle: bool = True
+
+while controle:
+    a = input("Digite o nome do aluno: ")
+    #func_pra_add_aluno()
+    
+    op = int(input("Deseja adicionar mais um aluno? Digite 1 pra sim 2 para não"))
+    if op == 2:
+        controle = False
+    else:
+        controle = True
+        
+        
+
+while True:
+    if 2 == 2:
+        break
