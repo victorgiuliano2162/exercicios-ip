3d68777f546522c3a39cbcc2d639cbc7e306b3cb
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-10-04 12:36:47 -0300

    corrigido verificação do primo

--- questao3.py
+++ questao3.py
@@ -8,8 +8,8 @@ imprima se ele é primo
 num = int(input("Digite o número para verificar se é primo: "))
 
 def calc_primo(num):
-    if num >= 0:
-        if num == 2 or num== 3 or num == 5 or num == 7:
+    if num > 0:
+        if num == 1 or num == 2 or num== 3 or num == 5 or num == 7:
             print(f"{num} é primo") 
         else:     
             if num % 2 != 0 and num % 3 != 0 and num % 5 != 0 and num % 7 != 0:
