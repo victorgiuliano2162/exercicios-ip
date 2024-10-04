5f3dc649f6bdf8236a6026e853d3333483943025
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-10-04 12:37:06 -0300

    questão concluída

--- questao1.py
+++ questao1.py
@@ -4,6 +4,17 @@
 informar o número de participantes
 cada participante recebe um valor de 1 a 5
 
+output:
+Quantos participantes?
+
+Númeração do participante 1: 2
+Númeração do participante 2: 3
+Númeração do participante 3: 4
+
+Total: 7
+
+O vencedor é: 
+
 '''
 import random
 
@@ -11,13 +22,37 @@ participantes_val = []
 
 
 def inf_participantes():
-    num_part = int(input("Informe o número de participantes: "))
-    popular_lista_participante()
+    num_part = int(input("Quantos participantes? "))
+    popular_lista_participante(num_part)
     
 
 def popular_lista_participante(num):
-    while num == 1:
+    cont = 1
+    if num == 1:
         print("Quantidade inválida de participantes. Escolha um valor maior que 1.")
-        inf_participantes()
-    if num > 1:
-        participantes_val.append(random.randint(1, num))
+        print("")
+    else:
+        while cont <= num:
+            participantes_val.append(random.randint(1, 5))
+            cont+=1
+        cont = 1
+        for part in participantes_val:
+            print(f"Númeração do participante: {cont}: {part}")
+            cont+= 1
+        
+def calc_winner2():
+    contador = len(participantes_val)
+    accum = sum(participantes_val)
+    winner = 1
+    while accum != 0:
+        accum = accum - 1
+        if winner > contador:
+            winner = 0
+        else:
+            winner+= 1
+            
+        if accum == 0:
+            print(f"O vencedor é o participante:a {winner}")
+            
+inf_participantes()
+calc_winner2()
